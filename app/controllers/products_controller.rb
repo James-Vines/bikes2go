d73136d48759734b6043f25057dd74b730d463ae
Jimv76 <Jimv76@me.com>
2017-01-10 16:07:08 +0000

    6.7: Debugging

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,7 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
+    #byebug
     @product = Product.new
   end
 
@@ -35,6 +36,7 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
+    byebug
     @product = Product.new(product_params)
 
     respond_to do |format|
